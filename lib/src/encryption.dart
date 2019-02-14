220da67cf7503a8fcc9d59cc344727ca01684825
Jogboms <jeremiahogbomo@gmail.com>
2019-02-14 07:50:20 +0100

    chore: integrityHash

--- lib/src/encryption.dart
+++ lib/src/encryption.dart
@@ -1,7 +1,7 @@
 import 'dart:convert' show Utf8Encoder, json;
 
 import 'package:convert/convert.dart' show hex;
-import 'package:crypto/crypto.dart' show md5;
+import 'package:crypto/crypto.dart' show md5, sha256;
 import 'package:meta/meta.dart' show required, visibleForTesting;
 import 'package:tripledes/tripledes.dart' show BlockCipher, DESEngine;
 
@@ -15,21 +15,31 @@ class Encryption {
 
   final String secretKey;
 
-  String encrypt({
-    @required Map<String, dynamic> data,
-  }) {
+  String encrypt(Map<String, dynamic> data) {
     return BlockCipher(DESEngine(), generateKey(secretKey))
         .encodeB64(json.encode(data));
   }
 
-  Map<String, dynamic> decrypt({
-    @required String data,
-  }) {
+  Map<String, dynamic> decrypt(String data) {
     final String _data = BlockCipher(
       DESEngine(),
       generateKey(secretKey),
     ).decodeB64(data);
     return json.decode(_data);
+  }
+
+  String integrityHash(Map<String, dynamic> data) {
+    final _keys = data.keys.toList()..sort();
+    final _hash = _keys.fold('', (String acc, String val) {
+      // don't include int hash
+      if (val == 'integrity_hash') {
+        return acc;
+      }
+      return acc + data[val].toString();
+    });
+    return hex.encode(
+      sha256.convert(const Utf8Encoder().convert(_hash + secretKey)).bytes,
+    );
   }
 
   @visibleForTesting
--- test/encryption_test.dart
+++ test/encryption_test.dart
@@ -6,6 +6,7 @@ const SECK = "FLWSECK-bb971402072265fb156e90a3578fe5e6-X";
 const RAW_DATA = <String, int>{"a": 1, "b": 2, "c": 3};
 const ENCRYPTED_DATA = 'tGXfuhcyL46cVsynJ79v+w==';
 const GENERATED_KEY = 'bb9714020722eb4cf7a169f2';
+const GENERATED_INTEGRITY_HASH = '01a9467aee23fa8988a0968bcf04b534c8a0c67ee97e2bcd61c0e90e72cf1699';
 
 const encryption = const Encryption(secretKey: SECK);
 
@@ -15,16 +16,16 @@ void main() {
       final res = encryption.generateKey(SECK);
       expect(res, GENERATED_KEY);
     });
+    test('integrityHash', () {
+      final res = encryption.integrityHash(RAW_DATA);
+      expect(res, GENERATED_INTEGRITY_HASH);
+    });
     test('encrypt', () {
-      final res = encryption.encrypt(
-        data: RAW_DATA,
-      );
+      final res = encryption.encrypt(RAW_DATA);
       expect(res, ENCRYPTED_DATA);
     });
     test('decrypt', () {
-      final res = encryption.decrypt(
-        data: ENCRYPTED_DATA,
-      );
+      final res = encryption.decrypt(ENCRYPTED_DATA);
       expect(res, RAW_DATA);
     });
   });
