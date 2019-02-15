b588388a42885917c2c985cd3872f2dfa875a578
Jogboms <jeremiahogbomo@gmail.com>
2019-02-15 22:37:22 +0100

    refactor(bank): clean up

--- lib/src/banks.dart
+++ lib/src/banks.dart
@@ -1,13 +1,28 @@
-import 'package:http/http.dart' as http;
+import 'package:ravepay/src/models/banks.dart';
+import 'package:ravepay/src/models/main.dart';
+import 'package:ravepay/src/models/response.dart';
 import 'package:ravepay/src/utils/endpoints.dart';
 import 'package:ravepay/src/utils/http_wrapper.dart';
+import 'package:ravepay/src/utils/log.dart';
 
 class Banks {
   Banks() : _http = HttpWrapper();
 
   final HttpWrapper _http;
 
-  Future<http.Response> getBanks() {
-    return _http.get(Endpoints.getBanks);
+  Future<Response<List<Bank>>> fetch() async {
+    final _response = Response<List<Bank>>(
+      await _http.get(Endpoints.getBanks),
+      onTransform: (dynamic data, _) {
+        return Model.generator<Bank>(
+          data,
+          (dynamic bank) => Bank.fromJson(bank),
+        );
+      },
+    );
+
+    Log().debug("Banks.fetch() -> Response", _response);
+
+    return _response;
   }
 }
