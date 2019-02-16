003b8e369a0e616ba2c4b6746815b06f552a40e3
Jogboms <jeremiahogbomo@gmail.com>
2019-02-16 12:36:41 +0100

    chore(example): implement banks & charge

--- example/lib/banks.dart
+++ example/lib/banks.dart
@@ -1,9 +1,12 @@
 import 'package:example_app/_constants.dart';
-import 'package:example_app/src/charge.dart' as charge;
 import 'package:ravepay/ravepay.dart';
+
+void fetch() async {
+  await Banks().fetch();
+}
 
 void main() async {
   Rave.init(production: false, publicKey: PUBK, secretKey: SECK);
 
-  charge.main();
+  fetch();
 }
